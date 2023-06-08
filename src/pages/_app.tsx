0961508addd8f7143b528ff11232e26714161bef
AndriiNemirov <nemirov.andrei@gmail.com>
2023-06-08 23:58:11 +0300

    route events

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,25 +3,55 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {NextPageWithLayout} from "@/types/util";
-import {SessionProvider} from "next-auth/react";
-
+import { NextPageWithLayout } from "@/types/util";
+import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { useEffect } from "react";
 const queryClient = new QueryClient();
 
 type AppPropsWithLayout = AppProps & {
-    Component: NextPageWithLayout;
+  Component: NextPageWithLayout;
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
 
-    const getLayout = Component.getLayout ?? ((page) => page);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    };
+
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleStop);
+    router.events.on("routeChangeError", handleStop);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleStop);
+      router.events.off("routeChangeError", handleStop);
+    };
+  }, [router]);
+
   return (
     <QueryClientProvider client={queryClient}>
-        <SessionProvider session={pageProps.session}>
-      <ToastContainer hideProgressBar pauseOnHover autoClose={2000} theme={'colored'} closeOnClick />
+      <SessionProvider session={pageProps.session}>
+        <ToastContainer
+          hideProgressBar
+          pauseOnHover
+          autoClose={2000}
+          theme={"colored"}
+          closeOnClick
+        />
         {getLayout(<Component {...pageProps} />)}
-        </SessionProvider>
-
+      </SessionProvider>
     </QueryClientProvider>
   );
 }
